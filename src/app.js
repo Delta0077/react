e35456128a65f210ff9fa732925aed3d889b386a
Delta0077 <adas0262@gmail.com>
2021-11-18 14:24:15 +0530

    Conditional rendering in JSX

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ console.log("App.js is running..........");
 var app = {
   title: "Indecision App",
   subTitle: "This is some info about the app.",
+  options: ["One", "Two"],
 };
 
 var template = (
   <div>
-    <h1>{app.title}</h1> <p>{app.subTitle}</p>
+    <h1>{app.title}</h1>
+    {app.subTitle && <p>{app.subTitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
     <ol>
       <li>Item 1</li>
       <li>Item 2</li>
@@ -17,15 +20,19 @@ var template = (
 
 var user = {
   name: "Mr Deep",
-  age: 26,
+  age: 25,
   location: "India",
 };
-
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
 var templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age:{user.age}</p>
-    <p>Location:{user.location}</p>
+    <h1>{user.name ? user.name : "Annonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age:{user.age}</p>}
+    {getLocation(user.location)}
   </div>
 );
 var appRoot = document.getElementById("app");
